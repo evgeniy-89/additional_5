b5c294b05fb8f7f186ee0cf6a32116970080ec11
Dzmitry Kazbiarovich <roman.evgeniy@gmail.com>
2017-09-28 23:10:52 +0300

    Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stringConfig = '';
+  let stack = [];
+  let character, bracePosition;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    stringConfig += bracketsConfig[i].join('');
+  }
+
+  for (let i = 0; character = str[i]; i++) {
+    bracePosition = stringConfig.indexOf(character);
+
+    if (bracePosition === -1) {
+      continue;
+    }
+
+    if (bracePosition % 2 === 0) {
+      stack.push(bracePosition + 1);
+    } else {
+      if (stack.pop() !== bracePosition) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
